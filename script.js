2dab4b25fbd341cfc946383683966b446a1edf8d
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2021-11-10 09:11:51 -0500

    commit

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 
-gsap.to(".soldier", {x: 700, y: -400, delay: 2, duration: 3, scale: 1.6, opacity: .6})
+gsap.to(".soldier", {x: 700, y: -300, delay: 2, duration: 3, scale: 1.6, opacity: .6})
 gsap.to(".soldier", {x: 1000, y: -600, delay: 5, duration: 3, scale: 3, opacity:0})
 
-gsap.to(".expltwo", {x: 200, y: -500, delay: 5.5, duration: .5, scale: 3, opacity: .3})
+gsap.to(".expltwo", {x: 200, y: -450, delay: 5.5, duration: .5, scale: 3, opacity: .3})
 gsap.to(".expltwo", {delay: 6, duration: .5, scale: 8, opacity: 0})
 
 gsap.to(".soldiertwo", {x: -50, y: -300, delay: 4, duration: 3, rotation: 10, opacity: .6})
 gsap.to(".soldiertwo", {x: 790, y: -400, delay: 7, duration: 4, scale: 1.2, rotation: 25, opacity: 0})
 
-gsap.to(".soldierthree", {x: 400, y: -430, delay: 9, duration: 4, scale: 3.5, rotation: 8, opacity: .6})
-gsap.to(".soldierthree", {x: -700, y: -500, delay: 13.5, duration: 6, scale: 6.5, rotation: -14, opacity: 0})
+gsap.to(".soldierthree", {x: 400, y: -380, delay: 8, duration: 4, scale: 3.5, rotation: 8, opacity: .6})
+gsap.to(".soldierthree", {x: -700, y: -500, delay: 12.5, duration: 6, scale: 6.5, rotation: -14, opacity: 0})
 
-gsap.to(".explone", {x: 250, y: -500, delay: 13.7, duration: .6, scale: 3, opacity: .3})
-gsap.to(".explone", {delay: 14.3, duration: .5, scale: 8, opacity: 0})
+gsap.to(".explone", {x: -250, y: -500, delay: 12.7, duration: .6, opacity: .6})
+gsap.to(".explone", {delay: 13.3, duration: .5, scale: 6, opacity: 0})
 
-gsap.to(".expltwo", {x: -50, y: -480, delay: 17.5, duration: .3, scale: 3, opacity: .3})
-gsap.to(".expltwo", {delay: 17.8, duration: .5, scale: 9, opacity: 0})
+gsap.to(".expltwo", {x: -50, y: -480, delay: 16.5, duration: .3, scale: 3, opacity: .3})
+gsap.to(".expltwo", {delay: 16.8, duration: .5, scale: 9, opacity: 0})
 
-gsap.to(".soldierfour", {x: -30, y: -400, delay: 15, duration: 3, scale: 3, opacity: .6})
-gsap.to(".soldierfour", {y: -500, delay: 18, duration: 3, scale: 6, opacity: 0})
+gsap.to(".soldierfour", {x: -30, y: -370, delay: 14, duration: 3, scale: 3, opacity: .6})
+gsap.to(".soldierfour", {y: -500, delay: 17, duration: 3, scale: 6, opacity: 0})
 
-gsap.to(".soldierfive", {x: -80, y: -300, delay: 19, duration: 3, scale: 2, opacity: .6})
-gsap.to(".soldierfive", {x: -600, y: -400, delay: 22, duration: 3, scale: 6, opacity: 0})
+gsap.to(".soldierfive", {x: -80, y: -400, delay: 18, duration: 3, scale: 2, opacity: .6})
+gsap.to(".soldierfive", {x: -600, y: -450, delay: 21, duration: 3, scale: 5, opacity: 0})
+
+gsap.to(".expltwo", {x: 200, y: -500, delay: 21.5, duration: .5, scale: 3, opacity: .3})
+gsap.to(".expltwo", {delay: 22, duration: .5, scale: 8, opacity: 0})
 
 gsap.to(".box", {duration: 1, delay: 20, scale: 2, stagger: .5})
 
