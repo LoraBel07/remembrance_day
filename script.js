ec9a6ab8120ece7976bf83cc870b0cdca72be4a7
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2021-11-10 11:33:18 -0500

    commit

--- script.js
+++ script.js
@@ -20,11 +20,33 @@ gsap.to(".expltwo", {delay: 16.8, duration: .5, scale: 9, opacity: 0})
 gsap.to(".soldierfour", {x: -30, y: -370, delay: 14, duration: 3, scale: 3, opacity: .6})
 gsap.to(".soldierfour", {y: -500, delay: 17, duration: 3, scale: 6, opacity: 0})
 
-gsap.to(".soldierfive", {x: -80, y: -400, delay: 18, duration: 3, scale: 2, opacity: .6})
+gsap.to(".soldierfive", {x: -80, y: -350, delay: 18, duration: 3, scale: 2, opacity: .6})
 gsap.to(".soldierfive", {x: -600, y: -450, delay: 21, duration: 3, scale: 5, opacity: 0})
 
 gsap.to(".expltwo", {x: 200, y: -500, delay: 21.5, duration: .5, scale: 3, opacity: .3})
 gsap.to(".expltwo", {delay: 22, duration: .5, scale: 8, opacity: 0})
 
-gsap.to(".box", {duration: 1, delay: 20, scale: 2, stagger: .5})
+const flovers = document.querySelector("#section_flovers");
+
+flovers.addEventListener("mouseover", startAnimation);
+
+function startAnimation() {
+    let move = 40;
+    gsap.to(".box", {duration: 2, delay: 1.5, scale: 5.5, stagger: .3, repeat: 2, y: function(i) {
+    if (i%2 === 1) return move
+    else return -move
+} })
+
+}
+
+
+
+// gsap.to(".box", {duration: 1, delay: 1, scale: 2, stagger: 1})
+
+// gsap.from(".one", {x:300, y:100, scale: 1.5, opacity:.5})
+// gsap.to(".one", {x:300, y:100, delay: 1, scale: 1.5})
+// gsap.to(".two", {x:400, y:100, scale: 1.5})
+// gsap.to(".three", {x:500, y:150, scale: 1.5})
+// gsap.to(".one", {x:600, y:170, scale: 1.5})
+// gsap.to(".two", {x:700, y:140, scale: 1.5})
 
