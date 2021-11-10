2ec36340d3c4ecc23f60b45b9f83ad06670ed023
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2021-11-09 21:33:33 -0500

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+gsap.to(".soldier", {x: 700, y: -400, delay: 2, duration: 3, scale: 1.6, opacity: .6})
+gsap.to(".soldier", {x: 1000, y: -600, delay: 5, duration: 3, scale: 3, opacity:0})
+
+gsap.to(".soldiertwo", {x: -50, y: -300, delay: 4, duration: 3, rotation: 10, opacity: .6})
+gsap.to(".soldiertwo", {x: 790, y: -400, delay: 7, duration: 4, scale: 1.2, rotation: 25, opacity: 0})
+
+gsap.to(".soldierthree", {x: 400, y: -430, delay: 9, duration: 4, scale: 3.5, rotation: 8, opacity: .6})
+gsap.to(".soldierthree", {x: -700, y: -500, delay: 13, duration: 4, scale: 6.5, rotation: -14, opacity: 0})
+
+
+
+gsap.to(".soldierfour", {x: -30, y: -400, delay: 15, duration: 3, scale: 3, opacity: .6})
+gsap.to(".soldierfour", {y: -500, delay: 18, duration: 3, scale: 6, opacity: 0})
+
+gsap.to(".box", {duration: 2, delay: 1.5, scale: 1.5, stagger: .2, repeat: 2})
+
